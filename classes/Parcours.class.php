223e63e7d936f1f83ed39b175c8994ed320a2d70
Etienne <etienne.couzon@gmail.com>
2020-10-27 20:30:05 +0100

    parcours

--- classes/Parcours.class.php
+++ classes/Parcours.class.php
@@ -1,5 +1,56 @@
 <?php
 class Parcours{
-	//A COMPLETER
 	
+	private $par_num;
+	private $par_km;
+	private $vil_num1;
+	private $vil_num2;
+	
+	public function __construct($valeurs = array()){
+		if (!empty($valeurs))
+				//print_r ($valeurs);
+				 $this->affecte($valeurs);
+	}
+	public function affecte($donnees){
+		foreach ($donnees as $attribut => $valeur){
+				switch ($attribut){
+						case 'par_num': $this->setPar_num($valeur); break;
+						case 'par_km';  $this->setPar_km($valeur); break;
+						case 'vil_num1': $this->setVil_num1($valeur); break;
+						case 'vil_num2': $this->setVil_num2($valeur); break;
+				}
+		}
+
+		public function setPar_num($id){
+			$this->par_num=$id;
+		}
+	
+		public function setPar_km($id){
+			$this->vil_num2=$id;
+		}
+		public function getPar_num(){
+			return $this->par_num;
+		}
+	
+		public function getPar_km(){
+			return $this->par_km;
+		}
+	
+
+		public function setVil_num1($id){
+			$this->vil_num1=$id;
+		}
+	
+		public function setVil_num2($id){
+			$this->vil_num2=$id;
+		}
+		public function getVil_num1(){
+			return $this->vil_num1;
+		}
+	
+		public function getVil_num2(){
+			return $this->vil_num2;
+		}
+	
+}
 }
--- include/pages/ajouterParcours.inc.php
+++ include/pages/ajouterParcours.inc.php
@@ -1,2 +1,31 @@
+<h1>Ajouter un parcours</h1>
+
 <?php
-//A COMPLETER
+$db= new Mypdo();
+$parcoursmanag= new ParcoursManager($db);
+?>
+
+<form id="saisieVille" method="post">
+Ville 1:
+<select name="ville">
+<option value="Limoges">Limoges</option> 
+Lyon
+</select>
+Ville 2:
+<select name="ville2">
+<option value="Bordeaux">Bordeaux</option> 
+</select>
+Nombre de kilomètre(s)
+<select name="km">
+256
+</select>
+  <input type="submit" id="valider" name="valider" value="valider">
+</form>
+
+<?php if (!empty($_POST['nom'])){
+          $nom=$_POST["nom"];
+          $req="insert into ville (vil_nom) values('$nom')";
+          $result=$db->prepare($req)->execute();
+
+          echo "La ville ".$_POST['nom']." a été ajoutée";
+}?>
