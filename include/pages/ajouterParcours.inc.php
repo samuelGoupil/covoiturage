00f18be6ea59ad6fd0736214a20715d4ad7364eb
Etienne <etienne.couzon@gmail.com>
2020-10-30 19:10:24 +0100

    parcours2

--- include/pages/ajouterParcours.inc.php
+++ include/pages/ajouterParcours.inc.php
@@ -5,27 +5,29 @@ $db= new Mypdo();
 $parcoursmanag= new ParcoursManager($db);
 ?>
 
-<form id="saisieVille" method="post">
+<form id="saisieParcours" method="post">
 Ville 1:
-<select name="ville">
-<option value="Limoges">Limoges</option> 
-Lyon
+<select name="ville1">
+<option name="Ville1">Limoges</option> 
 </select>
 Ville 2:
 <select name="ville2">
-<option value="Bordeaux">Bordeaux</option> 
+<option name="Ville2">Bordeaux</option> 
 </select>
 Nombre de kilomètre(s)
 <select name="km">
+<option name="km"></option> 
 256
 </select>
   <input type="submit" id="valider" name="valider" value="valider">
-</form>
-
-<?php if (!empty($_POST['nom'])){
-          $nom=$_POST["nom"];
-          $req="insert into ville (vil_nom) values('$nom')";
+<?php if (!empty($_POST['ville1']) and !empty($_POST[ville2]) and !empty($_POST["km"])){
+          $vil_num1=$_POST["Ville1"];
+          $vil_num2=$_POST["Ville2"];
+          $km=$_POST["km"];
+          $req="insert into parcours (par_km,vil_num1,vil_num2) values('$km','$vil_num1', '$vil_num2')";
           $result=$db->prepare($req)->execute();
 
-          echo "La ville ".$_POST['nom']." a été ajoutée";
+          echo "Le parcours a été ajoutée";
 }?>
+</form>
+
