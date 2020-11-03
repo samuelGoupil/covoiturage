0b6793600108790365c89fd3143696629a4614b2
Samuel Goupil <samgoups@hotmail.com>
2020-11-03 15:25:49 +0100

    supprimer et modifier personne

--- include/pages/ModifierPersonne.inc.php
+++ include/pages/ModifierPersonne.inc.php
@@ -1,1 +1,29 @@
+<h1>Selectionner la personne à modifier</h1>
 
+<?php
+$db= new Mypdo();
+$personnemanag= new PersonneManager($db);
+$req="select count(*) from personne";
+$result=$db->prepare($req)->execute();
+?>
+
+<table>
+  <tr>
+    <th>Numero</th>
+    <th>nom</th>
+    <th>prenom</th>
+  </tr>
+<?php
+  $listePersonne=$personnemanag->getList();
+  foreach($listePersonne as $personne){
+  ?>
+      <tr>
+        <td> <?php echo $personne ->getPer_num();?></td>
+        <td> <?php echo $personne ->getPer_nom();?></td>
+        <td> <?php echo $personne ->getPer_prenom();?></td>
+      </tr>
+
+  <?php
+  }
+  ?>
+</table>
