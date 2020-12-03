1653a5bd37b356dddfd6d550d587a88a74d1b26a
Samuel Goupil <samgoups@hotmail.com>
2020-12-03 15:09:53 +0100

    personnes

--- include/pages/ajouterPersonne.inc.php
+++ include/pages/ajouterPersonne.inc.php
@@ -48,36 +48,36 @@ empty($_POST['password'])){
 	$_SESSION["numpersonne"]=$personnemanag->AjouterPersonne($_POST['nom'], $_POST["prenom"], $_POST["tel"],
 	$_POST["email"], $_POST["login"], $_POST["password"] );
 
-		if (!empty($_POST['role'])){?>
-
-			<h2>Ajouter un étudiant</h2>
-			<form  method="post">
-				<label for="year">Année :</label>
-				<select  name="annee" value="Année 1">
-					<option value="Année 1">Année 1</option>
-					<option value="Année 2">Année 2</option>
-					<option value="Année spéciale">Année spéciale</option>
-					<option value="Licence Professionnelle"></option>
-				</select>
-				<label for="departement">Département :</label>
-				<select  name="departement" value="">
-					<option value="Génie civil">Génie civil</option>
-					<option value="Informatique">Informatique</option>
-					<option value="GEA">GEA</option>
-				</select>
-				<input type="submit" name="valider" value="Valider">
-			</form>
-
-			<?php
+		if (!empty($_POST['role'])){
 			$role=$_POST['role'];
+			echo $role;
 
-			if($role=="etudiant"){
+			if($role=="etudiant"){?>
+				<h2>Ajouter un étudiant</h2>
+				<form  method="post">
+					<label for="year">Année :</label>
+					<select  name="annee" value="Année 1">
+						<option value="1">Année 1</option>
+						<option value="2">Année 2</option>
+						<option value="3">Année spéciale</option>
+						<option value="4">Licence professionnelle</option>
+					</select>
+					<label for="departement">Département :</label>
+					<select  name="departement" value="">
+						<option value="???">Génie civil</option>
+						<option value="???">Informatique</option>
+						<option value="???">GEA</option>
+					</select>
+					<input type="submit" name="valider" value="Valider">
+				</form>
+				<?php
 				if(!empty($_POST['annee'])&&!empty($_POST['departement'])){
 					$etudiantmanag= new EtudiantManager($db);
-					$result=$etudiantmanag->AjouterEtudiant($_POST['anne'], $_POST["departement"]);
+					$result=$etudiantmanag->AjouterEtudiant($_POST["departement"],$_POST['anne']);
 					echo "L'étudiant a été ajouté.";
 				}
-			}else{
+			}
+			if($role=="personnel"){
 				?>
 				<h2>Ajouter un salarié</h2>
 				<form  method="post">
