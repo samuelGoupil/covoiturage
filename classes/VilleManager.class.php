d0cfe3097aa046a7666b041f70a09a47c0f9f35e
Samuel Goupil <samgoups@hotmail.com>
2020-12-13 12:46:24 +0100

    getVille

---
+++ classes/VilleManager.class.php
@@ -0,0 +1,55 @@
+<?php
+class VilleManager{
+
+
+	public function __construct($db){
+		$this->db =$db;
+	}
+
+	public function AjouterVille($vil_nom){
+		$req= "INSERT INTO VILLE (vil_nom)
+		VALUES ('$vil_nom')";
+		$execVille=$this->db->prepare($req)->execute();
+		return $execVille;
+	}
+
+	public function getList(){
+		$listeVille=array();
+		$sql="Select vil_num, vil_nom from ville order by vil_num";
+		$req=$this->db->query($sql);
+		while ($ville=$req->fetch(PDO::FETCH_OBJ)){
+			$listeVille[]=new Ville ($ville);
+		}
+		return $listeVille;
+		$req->closeCursor();
+	}
+
+	public function getVilleByID($id){
+		$req="SELECT * FROM VILLE V WHERE V.vil_num=$id";
+		$reqexec=$this->db->query($req);
+		while($ville = $reqexec->fetch(PDO::FETCH_OBJ)){
+			$villeRetour = new Ville($ville);
+		}
+		return $villeRetour;
+		$reqexec->closeCursor();
+
+	}
+	public function getVilleByNom($id){
+		$req="SELECT * FROM VILLE V WHERE V.vil_nom=$id";
+		$reqexec=$this->db->query($req);
+		while($ville = $reqexec->fetch(PDO::FETCH_OBJ)){
+			$villeRetour = new Ville($ville);
+		}
+		return $villeRetour;
+		$reqexec->closeCursor();
+
+	}
+
+	public function getVille($vil_num){
+		$req="SELECT vil_num, vil_nom FROM Ville WHERE vil_num='$vil_num'";
+		$req=$this->db->query($req);
+		$ville = $req->fetch(PDO::FETCH_ASSOC);
+		return new Ville($ville);
+	}
+
+}
